3ba44f90810f88289368ff2c5648cd78cdf058e9
Przemyslaw Rzeznik <przemyslaw.rzeznik@3vcorporate.com>
2016-10-24 16:03:06 +0100

    add another table to DB

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -30,6 +30,23 @@ class DatabaseSeeder extends Seeder
             User::create($user);
         }
 
+        DB::table('unknown_words')->delete();
+
+        // $users = array(
+        //         ['name' => 'Ryan Chenkie', 'email' => 'ryanchenkie@gmail.com', 'password' => Hash::make('secret')],
+        //         ['name' => 'Chris Sevilleja', 'email' => 'chris@scotch.io', 'password' => Hash::make('secret')],
+        //         ['name' => 'Holly Lloyd', 'email' => 'holly@scotch.io', 'password' => Hash::make('secret')],
+        //         ['name' => 'Adnan Kukic', 'email' => 'adnan@scotch.io', 'password' => Hash::make('secret')],
+        // );
+            
+        // // Loop through each user above and create the record for them in the database
+        // foreach ($users as $user)
+        // {
+        //     User::create($user);
+        // }
+
+
+
         Model::reguard();
  
     }
